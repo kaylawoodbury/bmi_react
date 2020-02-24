707900132c340b327f705289437a26030f29bb17
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-24 11:52:06 +0100

    update app.jsx and form.jsx to include state, function for updates to input fields

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import Form from "./components/Form";
 
 class App extends Component {
+  state = {
+    weight: "",
+    height: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
-    return <Form />;
+    return (
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
+        />
+      </div>
+    );
   }
 }
 
